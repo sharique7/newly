0c94456504ca2d45b123b48909b99bcda7b41dc1
Sharique <109796064+sharique7@users.noreply.github.com>
2023-07-19 13:13:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="container">
+      <div className="item1">Item1</div>
+      <div className='item2'>Item2</div>
+      <div className='item3'>Item3</div>
+    </div>
+  );
+  }
+export default App;
